94feb2e4048463718d649e7e5cf230e6df98b1cd
ChannyHong <channyhong@college.harvard.edu>
2020-11-19 14:21:47 +0900

    testing

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import torch
+import pandas as pd
 
 from facenet_pytorch import MTCNN, InceptionResnetV1
 
@@ -10,3 +11,23 @@ mtcnn = MTCNN(
 
 resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
 
+
+
+DIR_INPUT = "../kaggle-dataset/"
+DIR_IMAGES = DIR_INPUT + "Medical mask/Medical mask/Medical Mask/images/"
+
+
+
+df = pd.read_csv(DIR_INPUT + "train.csv")
+
+print(df.head())
+
+
+
+unq_values = df["name"].unique()
+print("Total Records: ", len(df))
+print("Unique Images: ",len(unq_values))
+
+null_values = df.isnull().sum(axis = 0)
+print("\n> Null Values in each column <")
+print(null_values)
