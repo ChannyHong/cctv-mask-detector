d4d60e98cfa4da076305c70e66540a7908761d7d
ChannyHong <channyhong@college.harvard.edu>
2020-11-19 14:22:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,6 @@ import pandas as pd
 
 from facenet_pytorch import MTCNN, InceptionResnetV1
 
-mtcnn = MTCNN(
-    image_size=160, margin=0, min_face_size=20,
-    thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
-    device=device
-)
 
 resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
 
