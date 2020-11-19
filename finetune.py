4b371626ef48b1f1938bf889bc329d46db41285c
ChannyHong <channyhong@college.harvard.edu>
2020-11-19 18:01:57 +0900

    f

--- finetune.py
+++ finetune.py
@@ -1,37 +1,3 @@
-'''
-
-import torch
-import pandas as pd
-
-from PIL import Image
-
-
-import matplotlib.pyplot as plt
-
-df = pd.read_csv(DIR_INPUT + "train.csv")
-
-print(df.head())
-
-# Null Values, Unique Values
-
-unq_values = df["name"].unique()
-print("Total Records: ", len(df))
-print("Unique Images: ",len(unq_values))
-
-null_values = df.isnull().sum(axis = 0)
-print("\n> Null Values in each column <")
-print(null_values)
-
-
-# Total Classes
-
-classes = df["classname"].unique()
-print("Total Classes: ",len(classes))
-print("\n> Classes <\n",classes)
-'''
-
-
-
 from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
 from torch.utils.data import DataLoader
@@ -88,42 +54,6 @@ for x, y in loader:
     image_copy.save("../kaggle-dataset/images_test/all/output{}.jpg".format(count))
 
     count+=1
-
-    #cv2.imwrite("../kaggle-dataset/images_test/all/output.jpg", image_copy)
-
-
-'''
-    if x_aligned is not None:
-        print('Face detected with probability: {}'.format(prob))
-        aligned.append(x_aligned)
-        names.append(dataset.idx_to_class[y])
-'''
-
-
-
-
-
-
-
-'''
-
-image_names = os.listdir(DIR_IMAGES)
-
-for image_name in images:
-
-
-	# 
-	boxes, _ = mtcnn.detect(image)
-
-	# draw faces
-
-
-'''
-
-
-
-
-
 
 
 
