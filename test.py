af27ff188e8125a210fb97ccd0273ca578f2321c
ChannyHong <channyhong@college.harvard.edu>
2020-11-19 16:50:11 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,18 @@
+'''
+
 import torch
 import pandas as pd
 
-from facenet_pytorch import MTCNN, InceptionResnetV1
+from PIL import Image
 
 
-resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
-
-
-
-DIR_INPUT = "../kaggle-dataset/"
-DIR_IMAGES = DIR_INPUT + "Medical mask/Medical mask/Medical Mask/images/"
-
-
+import matplotlib.pyplot as plt
 
 df = pd.read_csv(DIR_INPUT + "train.csv")
 
 print(df.head())
 
-
+# Null Values, Unique Values
 
 unq_values = df["name"].unique()
 print("Total Records: ", len(df))
@@ -26,3 +21,90 @@ print("Unique Images: ",len(unq_values))
 null_values = df.isnull().sum(axis = 0)
 print("\n> Null Values in each column <")
 print(null_values)
+
+
+# Total Classes
+
+classes = df["classname"].unique()
+print("Total Classes: ",len(classes))
+print("\n> Classes <\n",classes)
+'''
+
+
+
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import numpy as np
+import pandas as pd
+import os
+
+workers = 0 if os.name == 'nt' else 4
+
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('Running on device: {}'.format(device))
+
+
+
+mtcnn = MTCNN(
+    image_size=160, margin=0, min_face_size=20,
+    thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+    device=device, keep_all=True
+)
+
+
+resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+
+def collate_fn(x):
+    return x[0]
+
+dataset = datasets.ImageFolder('../kaggle-dataset/images_test')
+dataset.idx_to_class = {i:c for c, i in dataset.class_to_idx.items()}
+loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=workers)
+
+aligned = []
+names = []
+for x, y in loader:
+    x_aligned, prob = mtcnn(x, return_prob=True)
+    print x_aligned
+    if x_aligned is not None:
+        print('Face detected with probability: {:8f}'.format(prob))
+        aligned.append(x_aligned)
+        names.append(dataset.idx_to_class[y])
+
+
+
+
+
+
+
+
+
+'''
+
+image_names = os.listdir(DIR_IMAGES)
+
+for image_name in images:
+
+
+	# 
+	boxes, _ = mtcnn.detect(image)
+
+	# draw faces
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
