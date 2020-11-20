b020f91a8754827e45553ee5d38429d9c8216f0a
ChannyHong <channyhong@college.harvard.edu>
2020-11-20 14:52:36 +0900

    Update pixelate.py

--- pixelate.py
+++ pixelate.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 # Open Paddington
-img = Image.open("../kaggle-dataset/images_test/all/36-frame220.png")
+img = Image.open("../kaggle-dataset/images_test/all/36-frame220.jpg")
 
 # Resize smoothly down to 16x16 pixels
 imgNew = img.resize((1280,720),resample=Image.BILINEAR)
