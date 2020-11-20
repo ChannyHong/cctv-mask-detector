681cdc2d2fcefa0f1559d5d589dbae203cc1601e
ChannyHong <channyhong@college.harvard.edu>
2020-11-20 15:34:24 +0900

    Update pixelate.py

--- pixelate.py
+++ pixelate.py
@@ -5,7 +5,9 @@ img = Image.open("../kaggle-dataset/images_test/all/0870.jpg")
 
 # Resize smoothly down to 16x16 pixels
 #imgNew = img.resize((1280,720),resample=Image.BILINEAR)
-imgNew = img.resize((640,360),resample=Image.BILINEAR)
+#imgNew = img.resize((640,360),resample=Image.BILINEAR)
+imgNew = img.resize((320,180),resample=Image.BILINEAR)
+
 
 # Scale back up using NEAREST to original size
 result = imgNew.resize(img.size,Image.NEAREST)
