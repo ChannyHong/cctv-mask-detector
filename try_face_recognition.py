30d3593ae37fb714e225318e86706109182b4cd5
ChannyHong <channyhong@college.harvard.edu>
2020-11-20 14:15:53 +0900

    Update try_face_recognition.py

--- try_face_recognition.py
+++ try_face_recognition.py
@@ -1,7 +1,7 @@
 import face_recognition
 
 
-image = face_recognition.load_image_file("../kaggle-dataset/images_test/all/14-frame10.jpg")
+image = face_recognition.load_image_file("../kaggle-dataset/images_test/all/0870.jpg")
 
 
 face_locations = face_recognition.face_locations(image, model="cnn") # 'cnn' model is much better than not using it
