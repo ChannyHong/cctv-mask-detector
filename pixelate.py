27dc9a6deac9350e769b8f471eeac01e740a9208
ChannyHong <channyhong@college.harvard.edu>
2020-11-20 14:51:33 +0900

    Create pixelate.py

---
+++ pixelate.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+# Open Paddington
+img = Image.open("../kaggle-dataset/images_test/all/36-frame220.png")
+
+# Resize smoothly down to 16x16 pixels
+imgNew = img.resize((1280,720),resample=Image.BILINEAR)
+
+# Scale back up using NEAREST to original size
+result = imgNew.resize(img.size,Image.NEAREST)
+
+# Save
+result.save('result.png')
