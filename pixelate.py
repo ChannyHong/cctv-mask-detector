7e93fb15f7f9d30acc812c91c0ba14998225473b
ChannyHong <channyhong@college.harvard.edu>
2020-11-20 15:42:36 +0900

    Update pixelate.py

--- pixelate.py
+++ pixelate.py
@@ -1,12 +1,42 @@
 from PIL import Image
+import os
+
+
+# Open Paddington
+
+
+images = os.listdir('../kaggle-dataset/images/original/')
+
+for image in images:
+
+	img = Image.open("../kaggle-dataset/images/original/{}".format(image))
+
+	# Resize smoothly down to 16x16 pixels
+	imgNew = img.resize((1280,720),resample=Image.BILINEAR) # pixelated
+	#imgNew = img.resize((640,360),resample=Image.BILINEAR) # pixelated-x2
+	#imgNew = img.resize((320,180),resample=Image.BILINEAR) # pixelated-x4
+
+
+	# Scale back up using NEAREST to original size
+	result = imgNew.resize(img.size,Image.NEAREST)
+
+	# Save
+	result.save('../kaggle-dataset/images/pixelated/{}'.format(image))
+
+
+
+
+'''
+
+from PIL import Image
 
 # Open Paddington
 img = Image.open("../kaggle-dataset/images_test/all/0870.jpg")
 
 # Resize smoothly down to 16x16 pixels
-#imgNew = img.resize((1280,720),resample=Image.BILINEAR)
-#imgNew = img.resize((640,360),resample=Image.BILINEAR)
-imgNew = img.resize((320,180),resample=Image.BILINEAR)
+#imgNew = img.resize((1280,720),resample=Image.BILINEAR) # pixelated
+#imgNew = img.resize((640,360),resample=Image.BILINEAR) # pixelated-x2
+imgNew = img.resize((320,180),resample=Image.BILINEAR) # pixelated-x4
 
 
 # Scale back up using NEAREST to original size
@@ -14,3 +44,5 @@ result = imgNew.resize(img.size,Image.NEAREST)
 
 # Save
 result.save('../kaggle-dataset/images_test/all/pixelated.jpg')
+
+'''
