1cd8393f6ae0b9c353cb47182b90b28b411a3302
ChannyHong <channyhong@college.harvard.edu>
2020-12-10 14:49:57 +0900

    major updates

--- finetune.py
+++ finetune.py
@@ -1,150 +1,288 @@
-from torch import nn
-import torch
-from torch import tensor
 
-from facenet_pytorch import MTCNN
+import torch
+from torch import nn, tensor
+from torch.autograd import Variable
+
+import json
+import os
+from mtcnn import MTCNN
 from PIL import Image, ImageDraw
 
+from torchviz import make_dot
 
 
 BATCH_SIZE = 8
 
 
+def iou(gt_box, pred_box): # gt_box = [top_left_x, top_left_y, width, height], pred_box = [top_left_x, top_left_y, bottom_right_x, bottom_right_y]
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print('Running on device: {}'.format(device))
+	# Determine x, y coordinates of the two boxes
+	box1_x1 = torch.tensor(float(gt_box["x"]), requires_grad=True)
+	box1_x2 = torch.tensor(float(gt_box["x"]) + gt_box["width"], requires_grad=True)
+	box1_y1 = torch.tensor(float(gt_box["y"]), requires_grad=True)
+	box1_y2 = torch.tensor(float(gt_box["y"]) + gt_box["height"], requires_grad=True)
 
-
-mtcnn = MTCNN(
-    image_size=160, margin=0, min_face_size=20,
-    thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
-    device=device, keep_all=True
-)
-
-
-
-print(mtcnn)
-for param in mtcnn.parameters():
-	print(param)
+	box2_x1 = pred_box[0]
+	box2_x2 = pred_box[2]
+	box2_y1 = pred_box[1]
+	box2_y2 = pred_box[3]
 
 
-def iou(box1, box2): # box = [top_left_x, top_left_y, width, height]
-	
+	# determine the (x, y)-coordinates of the intersection rectangle
+	#xA = torch.max(torch.tensor([box1_x1, box2_x1]))
+	xA = torch.max(box1_x1, box2_x1)
+	yA = torch.max(box1_y1, box2_y1)
+	xB = torch.min(box1_x2, box2_x2)
+	yB = torch.min(box1_y2, box2_y2)
 
+	# compute the area of intersection rectangle
+	# interArea = max(0, xB - xA) * max(0, yB - yA)
+	interArea = torch.mul(torch.max(torch.tensor(0), torch.sub(xB, xA)), torch.max(torch.tensor(0), torch.sub(yB, yA)))
+
+	# compute the area of both the prediction and ground-truth
+	# rectangles
+	#boxAArea = (box1_x2 - box1_x1) * (box1_y2 - box1_y1)
+	boxAArea = torch.mul(torch.sub(box1_x2, box1_x1), torch.sub(box1_y2, box1_y1))
+
+	#boxBArea = (box2_x2 - box2_x1) * (box2_y2 - box2_y1)
+	boxBArea = torch.mul(torch.sub(box2_x2, box2_x1), torch.sub(box2_y2, box2_y1))
+
+	# compute the intersection over union by taking the intersection
+	# area and dividing it by the sum of prediction + ground-truth
+	# areas - the interesection area
+	#iou = interArea / float(boxAArea + boxBArea - interArea)
+	iou = torch.div(interArea, torch.sub(torch.add(boxAArea, boxBArea), interArea))
+	# return the intersection over union value
+	return iou
+
+
+
+
+
+def dist(gt_box, pred_box): # gt_box = [top_left_x, top_left_y, width, height], pred_box = [top_left_x, top_left_y, bottom_right_x, bottom_right_y]	
+
+	# Determine x, y coordinates of the two boxes
+	box1_x1 = torch.tensor(float(gt_box["x"]), requires_grad=True)
+	box1_x2 = torch.tensor(float(gt_box["x"]) + gt_box["width"], requires_grad=True)
+	box1_y1 = torch.tensor(float(gt_box["y"]), requires_grad=True)
+	box1_y2 = torch.tensor(float(gt_box["y"]) + gt_box["height"], requires_grad=True)
+
+	box2_x1 = pred_box[0]
+	box2_x2 = pred_box[2]
+	box2_y1 = pred_box[1]
+	box2_y2 = pred_box[3]
+
+	# Determine midpoints of each box, then calculate Euclidean distance
+	box1_x_mid = torch.div(torch.add(box1_x1, box1_x2), 2)
+	box1_y_mid = torch.div(torch.add(box1_y1, box1_y2), 2)
+
+	box2_x_mid = torch.div(torch.add(box2_x1, box2_x2), 2)
+	box2_y_mid = torch.div(torch.add(box2_y1, box2_y2), 2)
+
+	delta_x = torch.abs(torch.sub(box1_x_mid, box2_x_mid))
+	delta_y = torch.abs(torch.sub(box1_y_mid, box2_y_mid))
+
+	dist = torch.sqrt(torch.add(torch.square(delta_x), torch.square(delta_y)))
+
+	return dist
 
 
 
 def criterion(y_pred, y_data):
-	loss = 0.0
+	batch_iou_list = None
+	batch_dist_list = None
+	batch_diff_list = None
 
-	for y_hat, y_gt in zip(y_pred, y_data):
-		
-		bd_regression_losses = []
+	counter = 0
 
-		for gt_box in y_gt:
-			
-			
-			closest_prediction = TODO
+	for y_hat, y_gt in zip(y_pred, y_data): # iterating for each image
+		counter += 1
 
-			bd_regression_losses.append(TODO)
+		# no bounding box for either y_pred or y_gt
+		if y_hat is None or y_gt is None:
+			if y_hat is None and y_gt is None:
+				continue # just ignore the entry altogether
+			elif y_hat is None:
+				number_diff = torch.tensor([len(y_gt)]).type(torch.FloatTensor)
+				if batch_diff_list is None:
+					batch_diff_list = number_diff 
+				else:
+					batch_diff_list = torch.cat((batch_diff_list, number_diff))
+			elif y_gt is None:
+				number_diff = torch.tensor([len(y_hat)]).type(torch.FloatTensor)
+				if batch_diff_list is None:
+					batch_diff_list = number_diff 
+				else:
+					batch_diff_list = torch.cat((batch_diff_list, number_diff))
+
+		# normal case where there is bounding box for both y_pred and y_gt
+		else:
+			iou_values = None#torch.tensor([[]], requires_grad=True)
+			dist_values = None
+
+			for pred_box in y_hat: # iterating through each predicted boxes of this image 
+				
+				closest_prediction_iou = torch.tensor(-1.0, requires_grad=True) # cannot have negative IOU
+				closest_prediction_dist = torch.tensor(-1.0, requires_grad=True) # cannot have negative IOU
+
+				for gt_box in y_gt:
+					current_iou = iou(gt_box, pred_box)
+					current_dist = dist(gt_box, pred_box)
+
+					if current_iou > closest_prediction_iou:
+						closest_prediction_iou = current_iou
+
+					if current_dist > closest_prediction_dist:
+						closest_prediction_dist = current_dist
+
+				# Append to the iou_values list tensor 
+				closest_prediction_iou = torch.unsqueeze(closest_prediction_iou, 0)
+				if iou_values is None:
+					iou_values = closest_prediction_iou
+				else:
+					iou_values = torch.cat((iou_values, closest_prediction_iou))
+
+				# Append to the dist_values list tensor
+				closest_prediction_dist = torch.unsqueeze(closest_prediction_dist, 0)
+				if dist_values is None:
+					dist_values = closest_prediction_dist
+				else:
+					dist_values = torch.cat((dist_values, closest_prediction_dist))
 
 
 
+
+			image_average_iou = torch.mean(iou_values)
+			image_average_dist = torch.mean(dist_values)
+
+			image_num_diff = torch.tensor([abs(len(y_hat) - len(y_gt))]).float()
+			image_num_diff.requires_grad = True
+
+
+			image_average_iou = torch.unsqueeze(image_average_iou, 0)
+			if batch_iou_list is None:
+				batch_iou_list = image_average_iou
+			else:
+				batch_iou_list = torch.cat((batch_iou_list, image_average_iou))
+
+			image_average_dist = torch.unsqueeze(image_average_dist, 0)
+			if batch_dist_list is None:
+				batch_dist_list = image_average_dist
+			else:
+				batch_dist_list = torch.cat((batch_dist_list, image_average_dist))
+
+			if batch_diff_list is None:
+				batch_diff_list = image_num_diff 
+			else:
+				batch_diff_list = torch.cat((batch_diff_list, image_num_diff))
+
+
+
+	if batch_iou_list is None:
+		batch_iou_list = torch.tensor(0.0, requires_grad=True)
+	if batch_dist_list is None:
+		batch_dist_list = torch.tensor(0.0, requires_grad=True)
+
+	batch_avg_iou = torch.neg(torch.mean(batch_iou_list))
+	lambda_iou = Variable(torch.tensor(1.0), requires_grad=True)
+	final_iou = torch.mul(batch_avg_iou, lambda_iou)
+
+	batch_avg_dist = torch.mean(batch_dist_list)
+	lambda_dist = Variable(torch.tensor(1.0), requires_grad=True)
+	final_dist = torch.mul(batch_avg_dist, lambda_dist)
+
+	batch_avg_diff = torch.mean(batch_diff_list)
+	lambda_diff = Variable(torch.tensor(1.0), requires_grad=True)
+	final_diff = torch.mul(batch_avg_diff, lambda_diff)
+
+	
+	#loss is avg_iou + avg_diff + avg_dist
+	temp = torch.add(final_iou, final_dist)
+	loss = torch.add(temp, final_diff)
+	#loss = batch_avg_iou
+
+
+
+
+	return loss
 	# identify how 'off' the predictions were on the following factors
 	# 1. area of the unaccounted box, compared to the average area of size of the ground truths
 	# 2. Euclidean distance of the center point of the box to the closest ground truth box 
 
 
 
-	return loss
 
 
 
+def main():
+	device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+	print('Running on device: {}'.format(device))
 
-dataset_list = os.listdir(DATA_PATH)
-dataset_size = len(dataset_list)
+	mtcnn = MTCNN(
+	    image_size=160, margin=0, min_face_size=20,
+	    thresholds=[0.1, 0.3, 0.1], factor=0.709, post_process=True,
+	    device=device, keep_all=True
+	)
 
+	train_image_list = os.listdir("../labeled_data/image_metadata")
+	train_dataset_size = len(train_image_list)
 
-#criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(mtcnn.parameters(), lr=0.01)
+	train_image_list.sort()
 
+	#criterion = torch.nn.MSELoss(reduction='sum')
+	optimizer = torch.optim.SGD(mtcnn.parameters(), lr=0.001)
 
-# Training loop
-for epoch in range(500):
+	# Training loop
+	for epoch in range(2):
 
-	# start looking from the first image
-	iterations = dataset_size / BATCH_SIZE
+		# calculate how many iterations in the epoch
+		iterations = int(train_dataset_size/BATCH_SIZE)
 
-	for iteration in iterations:
+		# Train through every batch
+		for iteration_num in range(iterations):
+			x_data = []
+			y_data = []
 
-		x_data = []
-		y_data = []
-		batch_start_index = iteration*BATCH_SIZE
-		for i in range(batch_start_index, batch_start_index+BATCH_SIZE):
-			image = Image.TODO
-			x_data.append(image)
-			y_data.append(TODO)
+			batch_start_index = iteration_num*BATCH_SIZE
 
-		y_pred, _ = mtcnn.detect(x_data) # batch size, 4
-		loss = criterion(y_pred, y_data)
+			for i in range(batch_start_index, batch_start_index+BATCH_SIZE):
+				metadata_filename = train_image_list[i]
+				metadata_file_pointer = open(os.path.join("../labeled_data/image_metadata", metadata_filename), "r+")
+				metadata_dict = json.load(metadata_file_pointer)
+				metadata_file_pointer.close()
+				label_id = metadata_dict["label_id"]
+				
+				image_filename = metadata_filename[:-5]
+				image = Image.open(os.path.join("../labeled_data/images", image_filename))
+				x_data.append(image)
 
-		print(f'Epoch: {epoch} | Loss: {loss.item()} ')
+				label_filename = "{}.json".format(label_id)
+				label_file_pointer = open(os.path.join("../labeled_data/labels", label_filename), "r+")
+				label_dict = json.load(label_file_pointer)
+				label_file_pointer.close()
+				label_objects = label_dict["result"]["objects"]
+				labels = []
+				for label_object in label_objects:
+					labels.append(label_object["shape"]["box"])
+				y_data.append(labels)
 
-	    # Zero gradients, perform a backward pass, and update the weights.
-	    optimizer.zero_grad()
-	    loss.backward()
-	    optimizer.step()
+			y_pred, probs = mtcnn.detect_face_tensor(x_data) # (batch_size, 4), (batch_size, 1)
 
+			loss = criterion(y_pred, y_data)
+			
+			#model_arch = make_dot(loss)
+			#model_arch.format = 'png'
+			#model_arch.render("loss_graphs/epoch{}-iter{}-loss.png".format(epoch, iteration_num))
 
+			print('Epoch: {} | Iteration: {} | Loss: {} '.format(epoch, iteration_num, loss))
 
+			# Zero gradients, perform a backward pass, and update the weights.
+			optimizer.zero_grad()
+			loss.backward()
+			optimizer.step()
 
-
-
-class Model(nn.Module):
-    def __init__(self):
-        """
-        In the constructor we instantiate two nn.Linear module
-        """
-        super(Model, self).__init__()
-        self.linear = torch.nn.Linear(1, 1)  # One in and one out
-
-    def forward(self, x):
-        """
-        In the forward function we accept a Variable of input data and we must return
-        a Variable of output data. We can use Modules defined in the constructor as
-        well as arbitrary operators on Variables.
-        """
-        y_pred = self.linear(x)
-        return y_pred
-
-
-# our model
-model = Model()
-
-# Construct our loss function and an Optimizer. The call to model.parameters()
-# in the SGD constructor will contain the learnable parameters of the two
-# nn.Linear modules which are members of the model.
-criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(mtcnn.parameters(), lr=0.01)
-
-# Training loop
-for epoch in range(500):
-    # 1) Forward pass: Compute predicted y by passing x to the model
-    y_pred = model(x_data)
-
-    # 2) Compute and print loss
-    loss = criterion(y_pred, y_data)
-    print(f'Epoch: {epoch} | Loss: {loss.item()} ')
-
-    # Zero gradients, perform a backward pass, and update the weights.
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+	torch.save(mtcnn.state_dict(), "./test.pt")
 
 
-# After training
-hour_var = tensor([[4.0]])
-y_pred = model(hour_var)
-print("Prediction (after training)",  4, model(hour_var).data[0][0].item())
-
-
+if __name__ == "__main__":
+	main()
--- test.py
+++ test.py
@@ -1,126 +1,77 @@
-'''
-
 import torch
-import pandas as pd
-
-from PIL import Image
-
-
-import matplotlib.pyplot as plt
-
-df = pd.read_csv(DIR_INPUT + "train.csv")
-
-print(df.head())
-
-# Null Values, Unique Values
-
-unq_values = df["name"].unique()
-print("Total Records: ", len(df))
-print("Unique Images: ",len(unq_values))
-
-null_values = df.isnull().sum(axis = 0)
-print("\n> Null Values in each column <")
-print(null_values)
-
-
-# Total Classes
-
-classes = df["classname"].unique()
-print("Total Classes: ",len(classes))
-print("\n> Classes <\n",classes)
-'''
-
-
-
-from facenet_pytorch import MTCNN, InceptionResnetV1
-import torch
-from torch.utils.data import DataLoader
-from torchvision import datasets
-import numpy as np
-import pandas as pd
 import os
-
+#from facenet_pytorch import MTCNN
 from PIL import Image, ImageDraw
+import json
 
-workers = 0 if os.name == 'nt' else 4
-
+from mtcnn import MTCNN
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('Running on device: {}'.format(device))
 
-
-
 mtcnn = MTCNN(
     image_size=160, margin=0, min_face_size=20,
-    thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
-    device=device, keep_all=True
+    thresholds=[0.1, 0.3, 0.1], factor=0.709, post_process=True,
+    device=device, keep_all=True#, pretrained_model_path="test.pt"
 )
 
+train_image_list = os.listdir("../labeled_data/image_metadata")
+train_dataset_size = len(train_image_list)
 
-resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+train_image_list.sort()
 
-def collate_fn(x):
-    return x[0]
-
-dataset = datasets.ImageFolder('../kaggle-dataset/images')
-dataset.idx_to_class = {i:c for c, i in dataset.class_to_idx.items()}
-loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=workers)
-
-aligned = []
-names = []
-count = 1
-
-for image, label in loader:
-    
-    #x_aligned, prob = mtcnn(x, return_prob=True)
-    print(count, label)
-    boxes, _ = mtcnn.detect(image)
-
-    image_copy = image.copy()
-    draw = ImageDraw.Draw(image_copy)
-
-    if not boxes is None:
-	    for box in boxes:
-	        draw.rectangle(box.tolist(), outline=(255, 0, 0), width=3)
-
-    image_copy.save("../kaggle-dataset/boxed_images/{}/{}.jpg".format(label, f'{count:04}'))
-
-    count += 1
-
-    #cv2.imwrite("../kaggle-dataset/images_test/all/output.jpg", image_copy)
-
-
-'''
-    if x_aligned is not None:
-        print('Face detected with probability: {}'.format(prob))
-        aligned.append(x_aligned)
-        names.append(dataset.idx_to_class[y])
-'''
+#train_image_list.remove(".DS_Store")
+#image_list.sort()
 
 
 
 
+for i in range(train_dataset_size):
+	metadata_filename = train_image_list[i]
+	metadata_file_pointer = open(os.path.join("../labeled_data/image_metadata", metadata_filename), "r+")
+	metadata_dict = json.load(metadata_file_pointer)
+	metadata_file_pointer.close()
+	label_id = metadata_dict["label_id"]
 
 
-
-'''
-
-image_names = os.listdir(DIR_IMAGES)
-
-for image_name in images:
+	image_filename = metadata_filename[:-5]
+	image = Image.open(os.path.join("../labeled_data/images", image_filename))
+				
 
 
-	# 
+	image_boxed = image.copy()
+	draw = ImageDraw.Draw(image_boxed)
+
+	#x_data.append(image)
+
+	#output = mtcnn.detect_face_tensor(image)
 	boxes, _ = mtcnn.detect(image)
 
-	# draw faces
+	#print("boxes is tensor: ", torch.is_tensor(boxes))
+	#print("probs is tensor: ", torch.is_tensor(probs))
+
+	#print(image_file)
+	#print(output)
+
+
+	if not boxes is None:
+		for box in boxes:
+			draw.rectangle(box.tolist(), outline=(255, 0, 0), width=3)
+
+	image_boxed.save(os.path.join("../labeled_data/draw_boxes", image_filename))
+
+
 
 
 '''
+x_data.reverse()
 
+boxes, probs = mtcnn.detect(x_data)
 
+print("Boxes: ", boxes)
+print("Probs: ", probs)
 
-
+'''
 
 
 
