5bc0d3bbf1da2fe4ec370a0a968587a3b31ea4a1
ChannyHong <channyhong@college.harvard.edu>
2020-12-13 17:22:53 +0900

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -257,7 +257,7 @@ def main():
 
 	mtcnn = MTCNN(
 	    image_size=160, margin=0, min_face_size=20,
-	    thresholds=[0.4, 0.5, 0.5], factor=0.709, post_process=True,
+	    thresholds=[0.3, 0.4, 0.4], factor=0.709, post_process=True,
 	    device=device, keep_all=True#, pretrained_model_path="test.pt"
 	)
 
